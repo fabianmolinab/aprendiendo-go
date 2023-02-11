33eb6bbb39565c093765b170b0401e10fe424b84
Fabian Molina <molinafabian11b@gmail.com>
2023-02-11 03:59:21 -0500

    Se agrego asignacion

---
+++ 03-control-flujos/06-asignacion/main.go
@@ -0,0 +1,25 @@
+package main
+
+
+func main() {
+
+  a := 2
+  //Suma de asignación
+
+  a += 2 // 4 
+
+  //Resta de asignación 
+
+  a -= 2 // 2
+
+  //Multiplicación  de asignación
+
+  a *=  4 // 8
+
+  //División de asignación
+
+  a /= 2 // 1
+
+  // Modulo de asignacion 
+  a %= 2 //0
+}
