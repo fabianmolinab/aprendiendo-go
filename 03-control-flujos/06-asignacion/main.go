f3fb495a918f33bafed528b322cba798d54527b4
Fabian Molina <molinafabian11b@gmail.com>
2023-02-14 10:15:21 -0500

    cambios de estilos de archivos

--- 03-control-flujos/06-asignacion/main.go
+++ 03-control-flujos/06-asignacion/main.go
@@ -4,20 +4,20 @@ package main
 func main() {
 
   a := 2
-  //Suma de asignación
 
+  //Suma de asignación
   a += 2 // 4 
 
-  //Resta de asignación 
 
+  //Resta de asignación 
   a -= 2 // 2
 
-  //Multiplicación  de asignación
 
+  //Multiplicación  de asignación
   a *=  4 // 8
 
-  //División de asignación
 
+  //División de asignación
   a /= 2 // 1
 
   // Modulo de asignacion 
