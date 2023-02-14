154109fb1da6ff72dae2d430a8c76761e2407c9f
Fabian Molina <molinafabian11b@gmail.com>
2023-02-14 10:14:47 -0500

    for range

---
+++ 03-control-flujos/09-for-range/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+  nombres := [...]string{"fabian","nail","jeff"}
+
+ /**
+  //Recorre el arreglo
+  for i := 0; i < len(nombres); i ++ {
+    fmt.Println(nombres[i])
+  }
+ **/
+
+
+ //Recorre el arreglo pero en este caso se coloca `_` para solo imprimir el elemento
+  for _, elemento := range nombres {
+    fmt.Println( elemento)
+  }  
+}
